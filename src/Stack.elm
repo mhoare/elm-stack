732dbea38e54aec3cb921f6593ff99cb9784f4a3
Matthew <matthew@mhoare.co.uk>
2016-12-10 17:16:25 +0000

    Fix bug with empty stack

--- src/Stack.elm
+++ src/Stack.elm
@@ -37,10 +37,7 @@ toList (Stack stack) =
 -}
 push : a -> Stack a -> Stack a
 push item (Stack stack) =
-    if List.length stack /= 0 then
-        Stack (item :: stack)
-    else
-        Stack stack
+    Stack (item :: stack)
 
 
 {-| Removes the item at the top of the stack and returns it as the first item of a tuple.
